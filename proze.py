e6abd29051559c4c2f8e9652f3aa6936c3b580cf
Marshall Bowles <marshall@marshallbowles.com>
2018-08-27 17:24:53 -0700

    Refactor to split up methods

--- proze.py
+++ proze.py
@@ -5,12 +5,29 @@ from strategy.text import TextStrategy
 import lib.cli
 import lib.config
 
-comments = Comments()
 names = Names()
 
 
 class StrategyNotFoundError(Exception):
     pass
+
+
+def check_invalid_names(line, path, line_number):
+    """Check the line and warn if it contains invalid names.
+    @type  line: str
+    @param line: Proze formatted line.
+    @type  path: str
+    @param path: Path to the proze file being parsed.
+    @type  line_number: number
+    @param line_number: Current line number in the file being parsed.
+    """
+    invalid = names.find_invalid(line)
+    if invalid:
+        print(
+            'WARN: Invalid names found in {}[{}]: {}'.format(
+                path, line_number, invalid
+            )
+        )
 
 
 def determine_strategy(args, options):
@@ -31,41 +48,38 @@ def determine_strategy(args, options):
     )
 
 
+def execute_strategy(strategy, args, options):
+    """Compile the proze project using the stragegy.
+    @type  strategy: BaseStrategy
+    @param strategy: Strategy to use.
+    @type  args: object
+    @param args: Parsed command line args.
+    @type  options: DotMap
+    @param options: Compile options parsed from the config file.
+    """
+    comments = Comments()
+    with strategy.compile(args.output) as compiler:
+        for path in options.compile.order:
+            with open(path, 'r') as proze_file:
+                comments.reset()
+                line_number = 0
+                for line in proze_file:
+                    line_number = line_number + 1
+                    line = comments.remove(line)
+                    check_invalid_names(line, path, line_number)
+                    if line:
+                        compiler.write(line)
+
+
 def run():
     """Compile proze to target format."""
     args = lib.cli.parse()
     options = lib.config.load()
-    strategy = determine_strategy(options)
-    with strategy.compile(target_path) as compiler: #TODO output file name
-        # TODO iterate through all *.proze files in config
-        # TODO print message and exit if no files found to compile
-        with open(path, 'r') as proze_file:
-            comments.reset()
-            line_number = 0
-            for line in proze_file:
-                line_number = line_number + 1
-                line = comments.remove(line)
-                check_invalid_names(line, path, line_number)
-                if line:
-                    comipler.write(line)
-
-
-def check_invalid_names(line, path, line_number):
-    """Check the line and warn if it contains invalid names.
-    @type  line: str
-    @param line: Proze formatted line.
-    @type  path: str
-    @param path: Path to the proze file being parsed.
-    @type  line_number: number
-    @param line_number: Current line number in the file being parsed.
-    """
-    invalid = names.find_invalid(line)
-    if invalid:
-        print(
-            'WARN: Invalid names found in {}[{}]: {}'.format(
-                path, line_number, invalid
-            )
-        )
+    if not options.compile.order:
+        print('No proze files to compile.')
+    else:
+        strategy = determine_strategy(options)
+        execute_strategy(strategy, args, options)
 
 
 if __name__ == "__main__":
