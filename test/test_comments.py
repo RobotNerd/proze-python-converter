680af52a42bb1e0f8e74955a597610725f61d8ad
Marshall Bowles <marshall@marshallbowles.com>
2018-08-29 08:34:59 -0700

    Add comment test for line token hiding block token

--- test/test_comments.py
+++ test/test_comments.py
@@ -5,19 +5,25 @@ import unittest
 
 class TestComments(unittest.TestCase):
 
-    def test_no_comment(self):
-        """Test a proze line that doesn't have any comments."""
+    def test_line_hides_block(self):
+        """A block comment is hidden by a line comment."""
         lines = [
-            '"Furthermore," she said, "I would never take a deal like that."',
-            'Use the hashtag #withoutHoldings on that post.',
-            'Nothing looks right in here.',
-            '',
+            [
+                'abcd ## test of ### hidden block ### comment token',
+                'abcd',
+            ],
+            [
+                'abcd ## test of ### hidden block comment token',
+                'abcd',
+            ],
         ]
         comments = Comments()
         for line in lines:
-            self.assertEqual(comments.remove(line), line)
+            comments.reset()
+            self.assertEqual(comments.remove(line[0]), line[1])
 
     def test_multiple_blocks(self):
+        """Test lines that have multiple block comments."""
         lines = [
             [
                 'a ### test ### b ### test ### c',
@@ -37,9 +43,20 @@ class TestComments(unittest.TestCase):
             comments.reset()
             self.assertEqual(comments.remove(line[0]), line[1])
 
+    def test_no_comment(self):
+        """Test a proze line that doesn't have any comments."""
+        lines = [
+            '"Furthermore," she said, "I would never take a deal like that."',
+            'Use the hashtag #withoutHoldings on that post.',
+            'Nothing looks right in here.',
+            '',
+        ]
+        comments = Comments()
+        for line in lines:
+            self.assertEqual(comments.remove(line), line)
+
 
 # TODO test cases
-#   - line comment hiding block comment
 #   - block comment hiding line comment
 #   - block comment from previous line
 #       - current line entirely commented out by blokc
