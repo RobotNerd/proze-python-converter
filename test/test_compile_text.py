27888d0c93782f5ab34bb620ba9f531b7c90df44
Marshall Bowles <marshall@marshallbowles.com>
2018-09-06 07:41:20 -0700

    Merge text compile test cases into single file

--- test/test_compile_text.py
+++ test/test_compile_text.py
@@ -4,17 +4,27 @@ import os
 import proze
 import unittest
 
-
 PATH = 'test/sample/tmp/output.txt'
 
 
-class TestCompileMissing(unittest.TestCase):
+class TestCompileText(unittest.TestCase):
+
+    """Tests for compiling proze projects using the text strategy."""
 
     def setUp(self):
         try:
             os.remove(PATH)
         except OSError:
             pass
+
+    def test_compile_empty(self):
+        """A project with no config/proze files generates nothing."""
+        args = DotMap()
+        args.doctype = 'txt'
+        args.output = PATH[:-4]
+        args.path = 'test/sample/no_data'
+        proze.run(args)
+        self.assertFalse(os.path.isfile(PATH))
 
     def test_compile_missing(self):
         """A project config file has links to files that don't exist."""
@@ -25,4 +35,3 @@ class TestCompileMissing(unittest.TestCase):
         proze.run(args)
         with open(PATH) as f:
             self.assertEqual(f.read(), '')
-
