f7b00d34f9b0cfb51d87f4e6d4713fbb650e21af
Marshall Bowles <marshall@marshallbowles.com>
2018-09-06 21:19:05 -0700

    Add tests for tracking compilation state

--- lib/state.py
+++ lib/state.py
@@ -13,6 +13,10 @@ class State(object):
         # chapter, or section tag.
         self.is_first_paragraph = None
 
+        # When true, the next line of proze is the first paragraph
+        # after a new title, chapter, or section.
+        self._find_first_paragraph = True
+
         # True if italics is carried over from a previous line.
         self.is_italics = None
 
@@ -31,13 +35,42 @@ class State(object):
         self.is_italics = False
         self.is_previous_line_blank = False
         self.is_section = False
+    
+    def _starts_new_part(self, line):
+        """Check if the line starts a new title, chapter, or section.
+        @type  line: str
+        @param line: Proze line.
+        @rtype:  bool
+        @return: True if the line starts a title, chapter, or section.
+        """
+        starts_new = False
+        lowercase = line.lower()
+        if lowercase.startswith('title:'):
+            self.is_chapter = False
+            self.is_section = False
+            self._find_first_paragraph = True
+            starts_new = True
+        elif lowercase.startswith('chapter:'):
+            self.is_chapter = True
+            self.is_section = False
+            self._find_first_paragraph = True
+            starts_new = True
+        elif lowercase.startswith('section:') or lowercase.strip() == '---':
+            self.is_chapter = False
+            self.is_section = True
+            self._find_first_paragraph = True
+            starts_new = True
+        return starts_new
 
     def update(self, line):
         """Update the document state based on the current line.
         @type  line: str
         @param line: Proze line.
         """
-        # TODO check if a tag block (title, section, chapter, author, etc)
-        #   and set first paragraph, is_chapter, etc. accordingly
-        # TODO check if the line is blank
-        raise NotImplementedError
+        self.is_first_paragraph = False
+        if not line.strip():
+            self.is_previous_line_blank = True
+        elif not self._starts_new_part(line):
+            if self._find_first_paragraph:
+                self._find_first_paragraph = False
+                self.is_first_paragraph = True
---
+++ test/test_state.py
@@ -0,0 +1,82 @@
+from dotmap import DotMap
+import lib.state
+import unittest
+
+
+class TestState(unittest.TestCase):
+
+    """Tests for tracking state of compilation process."""
+
+    def test_previous_line_blank(self):
+        """Track state when previous line is blank."""
+        state = lib.state.State()
+        self.assertFalse(state.is_previous_line_blank)
+        state.update('')
+        self.assertTrue(state.is_previous_line_blank)
+        state.update('     ')
+        self.assertTrue(state.is_previous_line_blank)
+        state.update('\t')
+        self.assertTrue(state.is_previous_line_blank)
+        state.update('\t    \t   ')
+        self.assertTrue(state.is_previous_line_blank)
+        state.update('a')
+        self.assertTrue(state.is_previous_line_blank)
+        state.update('Chapter: test')
+        self.assertTrue(state.is_previous_line_blank)
+
+    def test_in_chapter_or_section(self):
+        """Not in a chapter or section."""
+        state = lib.state.State()
+        self.assertFalse(state.is_section)
+        self.assertFalse(state.is_chapter)
+        state.update('')
+        self.assertFalse(state.is_section)
+        self.assertFalse(state.is_chapter)
+        state.update('test a line')
+        self.assertFalse(state.is_section)
+        self.assertFalse(state.is_chapter)
+        state.update('Chapter: test')
+        self.assertFalse(state.is_section)
+        self.assertTrue(state.is_chapter)
+        state.update('Section: test')
+        self.assertTrue(state.is_section)
+        self.assertFalse(state.is_chapter)
+
+    def test_first_paragraph(self):
+        """Flag first line of the first paragraph.
+        Applies after a title, chapter, or section tag.
+        """
+        proze_line = 'a line of text'
+        state = lib.state.State()
+        self.assertFalse(state.is_first_paragraph)
+        state.update('')
+        self.assertFalse(state.is_first_paragraph)
+        state.update(proze_line)
+        self.assertTrue(state.is_first_paragraph)
+        state.update('')
+        state.update(proze_line)
+        self.assertFalse(state.is_first_paragraph)
+        state.update(proze_line)
+        self.assertFalse(state.is_first_paragraph)
+        state.update('')
+        state.update(proze_line)
+        state.update(proze_line)
+        self.assertFalse(state.is_first_paragraph)
+        state.update('')
+        state.update('Title: test')
+        state.update(proze_line)
+        self.assertTrue(state.is_first_paragraph)
+        state.update('')
+        state.update('Chapter: test')
+        state.update(proze_line)
+        self.assertTrue(state.is_first_paragraph)
+        state.update('')
+        state.update('Section: test')
+        state.update(proze_line)
+        self.assertTrue(state.is_first_paragraph)
+        state.update('')
+        state.update('---')
+        state.update(proze_line)
+        self.assertTrue(state.is_first_paragraph)
+        state.update(proze_line)
+        self.assertFalse(state.is_first_paragraph)
