6023e2addb9686c97bb8c1c342144e1acd2b7293
Marshall Bowles <marshall@marshallbowles.com>
2019-02-06 14:07:01 -0800

    Split tests for flagging first line of a paragraph

--- test/test_state.py
+++ test/test_state.py
@@ -2,6 +2,8 @@ from dotmap import DotMap
 import lib.state
 import unittest
 
+proze_line = 'a line of text'
+
 
 class TestState(unittest.TestCase):
 
@@ -41,31 +43,61 @@ class TestState(unittest.TestCase):
         state.update('   ')
         self.assertFalse(state.is_bold)
 
-    def test_first_paragraph(self):
-        """Flag first line of the first paragraph.
-        Applies after a title, chapter, or section tag.
-        """
-        proze_line = 'a line of text'
+    def test_first_paragraph_none_exists(self):
+        """No first paragraph found from beginning of file."""
         state = lib.state.State()
         self.assertFalse(state.is_first_paragraph)
         state.update('')
         self.assertFalse(state.is_first_paragraph)
+
+    def test_first_paragraph_beginning_of_file(self):
+        """First paragraph found from beginning of file, no markup."""
+        state = lib.state.State()
+        self.assertFalse(state.is_first_paragraph)
+        state.update('')
         state.update(proze_line)
         self.assertTrue(state.is_first_paragraph)
+
+    def test_first_paragraph_not_flag_second_paragraph(self):
+        """Second paragraph not flagged as being first."""
+        state = lib.state.State()
+        self.assertFalse(state.is_first_paragraph)
+        state.update('')
+        state.update(proze_line)
         state.update('')
         state.update(proze_line)
         self.assertFalse(state.is_first_paragraph)
+
+    def test_first_paragraph_second_line_of_paragraph(self):
+        """Second lines of paragraphs not flagged as being first line."""
+        state = lib.state.State()
+        self.assertFalse(state.is_first_paragraph)
+        state.update('')
+        state.update(proze_line)
         state.update(proze_line)
         self.assertFalse(state.is_first_paragraph)
         state.update('')
         state.update(proze_line)
         state.update(proze_line)
         self.assertFalse(state.is_first_paragraph)
+
+    def test_first_paragraph_after_title(self):
+        """Flag first line after title markup as first paragraph."""
+        state = lib.state.State()
+        self.assertFalse(state.is_first_paragraph)
+        state.update('')
+        state.update(proze_line)
+        state.update(proze_line)
         state.update('')
         state.update('Title: test')
         state.update(proze_line)
-        self.assertFalse(state.is_markup_line)
         self.assertTrue(state.is_first_paragraph)
+
+    def test_first_paragraph_multiple_structural_tags(self):
+        """Flag first paragraph after each structure markup tag."""
+        state = lib.state.State()
+        state.update('Title: test')
+        state.update(proze_line)
         state.update('')
         state.update('Chapter: test')
         state.update(proze_line)
