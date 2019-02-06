230de6bcf63f0cac996cba872e4d1aab346b3637
Marshall Bowles <marshall@marshallbowles.com>
2019-02-06 12:04:41 -0800

    Add placeholder tests for compiling full sample docs

--- test/test_compile_text.py
+++ test/test_compile_text.py
@@ -4,12 +4,14 @@ import proze
 import test.expected_text as expected
 import unittest
 
-PATH = 'test/sample/tmp/output.txt'
+OUTPUT_PATH = 'test/sample/tmp/output.txt'
 projects = DotMap()
 projects.dark_and_stormy = 'test/sample/dark-and-story'
 projects.feelings = 'test/sample/feelings'
 projects.missing = 'test/sample/missing_data'
 projects.no_data = 'test/sample/no_data'
+projects.pumpkins.project = 'test/sample/pumpkins'
+projects.pumpkins.expected = 'test/sample/pumpkins/pumpkins.txt'
 
 
 class TestCompileText(unittest.TestCase):
@@ -18,7 +20,7 @@ class TestCompileText(unittest.TestCase):
 
     def setUp(self):
         try:
-            os.remove(PATH)
+            os.remove(OUTPUT_PATH)
         except OSError:
             pass
 
@@ -26,27 +28,45 @@ class TestCompileText(unittest.TestCase):
         """A project with no config/proze files generates nothing."""
         args = DotMap()
         args.doctype = 'txt'
-        args.output = PATH[:-4]
+        args.output = OUTPUT_PATH[:-4]
         args.path = projects.no_data
         proze.run(args)
-        self.assertFalse(os.path.isfile(PATH))
+        self.assertFalse(os.path.isfile(OUTPUT_PATH))
 
     def test_compile_missing(self):
         """A project config file has links to files that don't exist."""
         args = DotMap()
         args.doctype = 'txt'
-        args.output = PATH[:-4]
+        args.output = OUTPUT_PATH[:-4]
         args.path = projects.missing
         proze.run(args)
-        with open(PATH) as f:
+        with open(OUTPUT_PATH) as f:
             self.assertEqual(f.read(), '')
 
 #    def test_compile_with_config(self):
 #        """Compile a prose project to text using the config file."""
 #        args = DotMap()
 #        args.doctype = 'txt'
-#        args.output = PATH[:-4]
+#        args.output = OUTPUT_PATH[:-4]
 #        args.path = projects.dark_and_stormy
 #        proze.run(args)
-#        with open(PATH) as f:
+#        with open(OUTPUT_PATH) as f:
 #            self.assertEqual(f.read(), expected.dark_and_stormy())
+
+    # def test_pumpkins(self):
+    #     """Compile the pumpkins sample project."""
+    #     args = DotMap()
+    #     args.doctype = 'txt'
+    #     args.output = OUTPUT_PATH[:-4]
+    #     args.path = projects.pumpkins.project
+    #     proze.run(args)
+    #     with open(OUTPUT_PATH, 'r') as output:
+    #         with open(projects.pumpkins.expected, 'r') as expected:
+    #             line1, line2 = output.readline(), expected.readline()
+    #             while (
+    #                 line1 == line2 and
+    #                 line1 is not None and
+    #                 line2 is not None
+    #             ):
+    #                 line1, line2 = output.readline(), expected.readline()
+    #             self.assertEqual(line1, line2)
