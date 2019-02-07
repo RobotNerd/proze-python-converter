db12d1839115b4e554abd357f970e8cd0ac9ecc4
Marshall Bowles <marshall@marshallbowles.com>
2019-02-06 17:15:48 -0800

    Split up state tests into multiple files

---
+++ test/state/test_previous_line_blank.py
@@ -0,0 +1,28 @@
+from dotmap import DotMap
+import lib.state
+import unittest
+
+proze_line = 'a line of proze text'
+
+
+class TestStatePreviousLineBlank(unittest.TestCase):
+
+    """Check if previous line is considered to be a blank line."""
+
+    def test_previous_line_blank(self):
+        """Track state when previous line is blank."""
+        state = lib.state.State()
+        self.assertTrue(state.is_previous_line_blank)
+        state.update('')
+        self.assertTrue(state.is_previous_line_blank)
+        state.update('     ')
+        self.assertTrue(state.is_previous_line_blank)
+        state.update('\t')
+        self.assertTrue(state.is_previous_line_blank)
+        state.update('\t    \t   ')
+        self.assertTrue(state.is_previous_line_blank)
+        state.update('a')
+        self.assertFalse(state.is_previous_line_blank)
+        state.update('Chapter: test')
+        self.assertTrue(state.is_previous_line_blank)
+        self.assertFalse(state.is_markup_line)
