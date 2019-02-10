d227c222400f963ec08a77a23f3a5cfbc4d7c873
Marshall Bowles <marshall@marshallbowles.com>
2019-02-10 15:05:13 -0800

    Refactor state constructors and reset methods

--- lib/state.py
+++ lib/state.py
@@ -9,21 +9,17 @@ class MarkupState(object):
     """Track state related to structural markup."""
 
     def __init__(self):
-        # True if inside a chapter.
-        self.is_chapter = None
-        # True if the current line starts with a structural markup token.
-        self.is_markup_line = None
-        # True if inside a section.
-        self.is_section = None
-        # The markup token value. None if current line isn't a markup token.
-        self.token = None
         self.reset()
 
     def reset(self):
         """Reset all state values to default."""
+        # True if inside a chapter.
         self.is_chapter = False
+        # True if the current line starts with a structural markup token.
         self.is_markup_line = False
+        # True if inside a section.
         self.is_section = False
+        # The markup token value. None if current line isn't a markup token.
         self.token = None
 
     def check_markup(self, line, previous_line):
@@ -74,14 +70,12 @@ class PreviousLine(object):
     """State of the previous line of the proze file."""
 
     def __init__(self):
-        # True if the line can be treated as a blank line.
-        is_blank = None
-        # True if the line contained a structural markup tag.
-        is_structural_markup = None
         self.reset()
 
     def reset(self):
+        # True if the line can be treated as a blank line.
         self.is_blank = True
+        # True if the line contained a structural markup tag.
         self.is_structural_markup = False
 
     def update(self, is_blank, is_structural_markup):
@@ -97,18 +91,6 @@ class State(object):
         # When true, the next line of proze is the first paragraph
         # after a new title, chapter, or section.
         self._find_first_paragraph = True
-        # Track the indentation level for block quotes.
-        self._indent_leading_whitespace = []
-        self.indent_level = 0
-        # True if line is blank.
-        self._is_blank = None
-        # True if bold is carried over from a previous line.
-        self.is_bold = None
-        # True if currently in the first paragraph after a title,
-        # chapter, or section tag.
-        self.is_first_paragraph = None
-        # True if italics is carried over from a previous line.
-        self.is_italics = None
         # Track state of structural markup tags.
         self.markup = MarkupState()
         # Track state of the previous line of proze.
@@ -147,11 +129,17 @@ class State(object):
         """Reset all state values to default."""
         self.markup.reset()
         self.previous_line.reset()
+        # Track the indentation level for block quotes.
         self._indent_leading_whitespace = []
         self.indent_level = 0
+        # True if line is blank.
         self._is_blank = True
+        # True if bold is carried over from a previous line.
         self.is_bold = False
+        # True if currently in the first paragraph after a title,
+        # chapter, or section tag.
         self.is_first_paragraph = False
+        # True if italics is carried over from a previous line.
         self.is_italics = False
 
     def _toggle_bold_and_italics(self, line):
